eb28e84ac52d40ab2faadf945d4e33dd1e9a918e
amandeep-saluja <amandeepsaluja25@gmail.com>
2022-02-26 15:39:45 +0530

    Day-15

--- Practice/Section 8 Bit Manipulation/Ex_26_Earth_Levels.cpp
+++ Practice/Section 8 Bit Manipulation/Ex_26_Earth_Levels.cpp
@@ -1,11 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int earthLevel(int k)
+{
+    int c = 0;
+    while (k > 0)
+    {
+        if(k&1)
+            c++;
+        k = k >> 1;
+    }
+    return c;
+}
+
+int main()
+{
+
+    int enemy = 13;
+
+    int n = earthLevel(enemy);
+
+    cout << "No. of jumps: " << n << endl;
+
+    return 0;
+}
+
 /*
 
 Earth Levels!
 
-The Planet Earth is under a threat from the aliens of the outer space and 
-your task is to defeat an enemy who is N steps above you (assume yourself to be at ground level i.e. at the 0th level). 
-You can take jumps in power of 2. In order to defeat the enemy as quickly as possible you have to reach 
-the Nth step in minimum moves possible. 
+The Planet Earth is under a threat from the aliens of the outer space and
+your task is to defeat an enemy who is N steps above you (assume yourself to be at ground level i.e. at the 0th level).
+You can take jumps in power of 2. In order to defeat the enemy as quickly as possible you have to reach
+the Nth step in minimum moves possible.
 
 Input Format
 
@@ -15,10 +42,10 @@ Output Format
 
 Return an integer representing minimum jumps.
 
-Sample Input 
+Sample Input
 7
 
-Sample Output 
+Sample Output
 
 3
 
---
+++ Practice/Section 8 Bit Manipulation/Ex_27_Subset_Sum_Queries.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<bool> subsetSum(vector<int> num, vector<int> query)
+{
+    int N = query.size();
+    vector<bool> result;
+
+    vector<int> prefixSum;
+
+    for (int i = 0; i < num.size(); i++)
+    {
+        for (int j = i + 1; j < num.size(); j++)
+        {
+            int sum = num[i] + num[j];
+
+            prefixSum.push_back(sum);
+        }
+    }
+
+    for (int x : prefixSum)
+    {
+        cout << x << "  ";
+    }
+    cout << endl;
+
+    for (int i = 0; i < N; i++)
+    {
+        bool found = false;
+        for (int j = 0; j < prefixSum.size(); j++)
+        {
+            if ((prefixSum[j] ^ query[i]) == 0)
+            {
+                found = true;
+            }
+        }
+        result.push_back(found);
+    }
+
+    return result;
+}
+
+int main()
+{
+
+    vector<int> num = {1, 2, 3};
+    vector<int> query = {5, 3, 8};
+
+    vector<bool> res = subsetSum(num, query);
+
+    for (bool x : res)
+    {
+        cout << x << " ";
+    }
+
+    return 0;
+}
+
+/*
+
+Subset Sum Queries
+
+Given an array and N number of queries, where in each query you have to check whether
+ a subset whose sum is equal to given number exists in the array or not.
+
+ Input Format
+
+ In the function two vectors num and query are passed.
+
+ Output Format
+
+ Return a bool vector of size N representing each query.
+
+ Sample Input
+
+ num[]   = {1, 2, 3}
+ query[] = {5, 3, 8}
+
+ Sample Output
+
+ {Yes, Yes, No}
+
+ Explanation
+
+ There is a subset with sum 5, subset is {2, 3}
+ There is a subset with sum 3, subset is {1, 2}
+ There is no subset with sum 8.
+
+*/
