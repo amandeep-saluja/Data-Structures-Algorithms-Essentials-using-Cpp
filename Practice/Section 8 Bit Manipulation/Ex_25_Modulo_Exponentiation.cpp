c4a3b899be02cef5d2887362962b33f4f7d3a300
amandeep-saluja <amandeepsaluja25@gmail.com>
2022-02-26 13:06:05 +0530

    Day-15

--- Practice/Section 8 Bit Manipulation/Ex_25_Modulo_Exponentiation.cpp
+++ Practice/Section 8 Bit Manipulation/Ex_25_Modulo_Exponentiation.cpp
@@ -3,14 +3,34 @@ using namespace std;
 
 int power(int x, int y, int mod)
 {
-    return (x ^ y) % mod;
+    int ans = 1;
+
+    x = x % mod;
+
+    if (x == 0)
+    {
+        return 0;
+    }
+
+    while (y > 0)
+    {
+        if (y & 1)
+        {
+            ans = (ans * x) % mod;
+        }
+
+        x = (x * x) % mod;
+        y = y >> 1;
+    }
+
+    return ans;
 }
 
 int main()
 {
-
+    // 12 -> 00001100
+    // 25 -> 00011001
     int x = 12, y = 25, mod = 10007;
-
     cout << power(x, y, mod) << endl;
 
     return 0;
---
+++ Practice/Section 8 Bit Manipulation/Ex_26_Earth_Levels.cpp
@@ -0,0 +1,27 @@
+/*
+
+Earth Levels!
+
+The Planet Earth is under a threat from the aliens of the outer space and 
+your task is to defeat an enemy who is N steps above you (assume yourself to be at ground level i.e. at the 0th level). 
+You can take jumps in power of 2. In order to defeat the enemy as quickly as possible you have to reach 
+the Nth step in minimum moves possible. 
+
+Input Format
+
+In the function an integer is passed.
+
+Output Format
+
+Return an integer representing minimum jumps.
+
+Sample Input 
+7
+
+Sample Output 
+
+3
+
+Explanation0 -> 4 -> 6 -> 7
+
+*/
