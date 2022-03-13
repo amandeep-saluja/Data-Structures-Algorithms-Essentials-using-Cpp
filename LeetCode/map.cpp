1395c1fc828040f8b5ea89a56dd0809066245f3a
amandeep-saluja <amandeepsaluja25@gmail.com>
2022-03-13 18:16:02 +0530

    Day-30

--- LeetCode/map.cpp
+++ LeetCode/map.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
 #include <map>
-#include <iterator>
 using namespace std;
+
+void printMap1(map<int, int> &m)
+{
+    if (m.size() == 0)
+    {
+        cout << "No element found." << endl;
+        return;
+    }
+
+    map<int, int>::iterator it;
+
+    for (it = m.begin(); it != m.end(); it++)
+    {
+        cout << it->first << " " << it->second << endl;
+    }
+}
+
+void printMap2(map<int, int> &m)
+{
+    if (m.size() == 0)
+    {
+        cout << "No element found." << endl;
+        return;
+    }
+
+    for (auto &p : m)
+    {
+        cout << p.first << " " << p.second << endl;
+    }
+}
 
 int square(int x)
 {
@@ -14,12 +43,27 @@ int main()
 
     for (int i = 1; i < 10; i++)
     {
-        squares.insert(i, square(i));
+        squares.insert({i, square(i)}); // O[logn]
     }
 
-    
+    // printMap1(squares);
+    // printMap2(squares);
 
-    cout << squares << endl;
+    map<int, int>::iterator it = squares.find(3);
+
+    // O[logn]
+    if (it != squares.end())
+    {
+        cout << it->first << " " << it->second << endl;
+        squares.erase(it);
+    }
+    else
+    {
+        cout << "Not found" << endl;
+    }
+    squares.clear();
+
+    printMap2(squares);
 
     return 0;
 }
