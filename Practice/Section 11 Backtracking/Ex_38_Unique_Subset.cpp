82b0808a172a650127f568791afb5a825665f3ed
amandeep-saluja <amandeepsaluja25@gmail.com>
2022-03-26 22:08:37 +0530

    Day-37

--- Practice/Section 11 Backtracking/Ex_38_Unique_Subset.cpp
+++ Practice/Section 11 Backtracking/Ex_38_Unique_Subset.cpp
@@ -1,31 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
+set<vector<int>> s;
 
-void helper(vector<int> nums, set<vector<int>> &subsets, vector<int> &v, int j)
+void helper(vector<int> nums, vector<int> &v, int i)
 {
     // base case
     int n = nums.size();
-    if (j >= n)
+    if (i == n)
     {
-        subsets.insert(v);
         sort(v.begin(), v.end());
+        s.insert(v);
         return;
     }
 
     // recursive case
-    // cout << "j: " << j << endl;
-    v.push_back(nums[j]);
-    helper(nums, subsets, v, j + 1);
+    v.push_back(nums[i]);
+    helper(nums, v, i + 1);
     v.pop_back();
-    helper(nums, subsets, v, j + 1);
+    helper(nums, v, i + 1);
 }
 
 vector<vector<int>> uniqueSubsets(vector<int> nums)
 {
-    set<vector<int>> subsets;
+    s.clear();
     vector<int> v;
-    helper(nums, subsets, v, 0);
-    vector<vector<int>> sol(subsets.begin(), subsets.end());
+    helper(nums, v, 0);
+    vector<vector<int>> sol;
+    for (auto x : s)
+    {
+        sol.push_back(x);
+    }
     return sol;
 }
 
