3d911a9f1af91539caf833702647ad40055b3ad6
amandeep-saluja <amandeepsaluja25@gmail.com>
2022-03-27 12:31:23 +0530

    Day-38

---
+++ Practice/Section 12 OOP/Product.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Product
+{
+private:
+    int id;
+    char name[100];
+
+public:
+    int mrp;
+    int selling_price;
+};
+
+int main()
+{
+
+    Product camera;
+    camera.mrp = 100;
+    camera.selling_price = 200;
+    cout << sizeof(camera) << endl;
+    cout << "MRP: " << camera.mrp << endl;
+    cout << "Selling Price: " << camera.selling_price << endl;
+
+    return 0;
+}
