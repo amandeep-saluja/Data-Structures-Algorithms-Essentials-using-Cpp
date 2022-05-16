36f84843fbb879a0e8ee0c0795a073fc460fbc97
amandeep-saluja <amandeepsaluja25@gmail.com>
2022-05-16 23:37:01 +0530

    Day-52

--- Practice/Section 15 Queue/Queue_test.cpp
+++ Practice/Section 15 Queue/Queue_test.cpp
@@ -6,16 +6,18 @@ int main()
 {
 
     Queue q;
-    // cout << q.front() << endl;
     q.push(1);
     q.push(2);
     q.push(3);
     q.push(4);
     q.push(5);
-    q.printQueue();
-
     q.pop();
-    q.printQueue();
+    q.push(1);
+    while (!q.isEmpty())
+    {
+        cout << q.front() << " ";
+        q.pop();
+    }
 
     return 0;
 }
