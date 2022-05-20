c9a9f39b1d69c04e69723d2200287f303fa8d263
amandeep-saluja <amandeepsaluja25@gmail.com>
2022-05-20 23:40:07 +0530

    Day-54

---
+++ Practice/SEction 16 Binary Tree/Inorder_traversal.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int d)
+    {
+        data = d;
+        left = right = NULL;
+    }
+};
+
+// Preorder build of the tree -> Root, Left, Right Tree
+Node *buildTree()
+{
+    int d;
+    cin >> d;
+    if (d == -1)
+    {
+        return NULL;
+    }
+    Node *n = new Node(d);
+    n->left = buildTree();
+    n->right = buildTree();
+
+    return n;
+}
+
+// Inorder Traversal -> Left, Root, Right
+void printInorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    printInorder(root->left);
+    cout << root->data << " ";
+    printInorder(root->right);
+}
+
+int main()
+{
+    Node *root = buildTree();
+    printPreorder(root);
+
+    return 0;
+}
+// 1 2 4 -1 -1 5 7 -1 -1 -1 3 -1 6 -1 -1
