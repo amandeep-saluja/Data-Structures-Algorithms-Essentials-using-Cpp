c67f9cc86f2773768da113d2336af857c539a895
amandeep-saluja <amandeepsaluja25@gmail.com>
2022-05-20 23:41:26 +0530

    Day-54

---
+++ Practice/SEction 16 Binary Tree/Postorder_traversal.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int d)
+    {
+        data = d;
+        left = right = NULL;
+    }
+};
+
+// Preorder build of the tree -> Root, Left, Right Tree
+Node *buildTree()
+{
+    int d;
+    cin >> d;
+    if (d == -1)
+    {
+        return NULL;
+    }
+    Node *n = new Node(d);
+    n->left = buildTree();
+    n->right = buildTree();
+
+    return n;
+}
+
+void printPostOrder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    printPostOrder(root->left);
+    printPostOrder(root->right);
+    cout << root->data << " ";
+}
+
+int main()
+{
+    Node *root = buildTree();
+    printPostOrder(root);
+
+    return 0;
+}
+// 1 2 4 -1 -1 5 7 -1 -1 -1 3 -1 6 -1 -1
