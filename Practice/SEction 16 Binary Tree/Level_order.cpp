1bb791790bf09d64cd2835a9449931589d89c53d
amandeep-saluja <amandeepsaluja25@gmail.com>
2022-05-21 23:32:00 +0530

    Day-55

--- Practice/SEction 16 Binary Tree/Level_order.cpp
+++ Practice/SEction 16 Binary Tree/Level_order.cpp
@@ -32,13 +32,93 @@ Node *buildTree()
     return n;
 }
 
-void levelOrder(Node *root, queue<Node*> q)
+Node *buildLevelOrderTree()
 {
-    if (root == NULL)
+    int d;
+    cin >> d;
+
+    if (d == -1)
     {
-        return;
+        return NULL;
     }
 
+    queue<Node *> q;
+    Node *root = new Node(d);
+    q.push(root);
+
+    while (!q.empty())
+    {
+        Node *temp = q.front();
+        q.pop();
+
+        cin >> d;
+        if (d != -1)
+        {
+            Node *p = new Node(d);
+            temp->left = p;
+            q.push(p);
+        }
+        cin >> d;
+        if (d != -1)
+        {
+            Node *p = new Node(d);
+            temp->right = p;
+            q.push(p);
+        }
+    }
+
+    return root;
+}
+
+void levelOrder(Node *root)
+{
+    queue<Node *> q;
+    q.push(root);
+    q.push(NULL);
+
+    while (!q.empty())
+    {
+        Node *temp = q.front();
+        if (temp == NULL)
+        {
+            cout << endl;
+            q.pop();
+            // insert a new null for the next level
+            if (!q.empty())
+            {
+                q.push(NULL);
+            }
+        }
+        else
+        {
+            q.pop();
+            cout << temp->data << " ";
+            if (temp->left != NULL)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right != NULL)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+}
+
+int main()
+{
+    Node *root = buildLevelOrderTree();
+    levelOrder(root);
+
+    return 0;
+}
+// 1 2 4 -1 -1 5 7 -1 -1 -1 3 -1 6 -1 -1
+
+/*
+
+void levelOrder(Node *root)
+{
+    queue<Node *> q;
     q.push(root);
     q.push(NULL);
 
@@ -59,12 +139,4 @@ void levelOrder(Node *root, queue<Node*> q)
     }
 }
 
-int main()
-{
-    Node *root = buildTree();
-    queue<Node*> q;
-    levelOrder(root, q);
-
-    return 0;
-}
-// 1 2 4 -1 -1 5 7 -1 -1 -1 3 -1 6 -1 -1
+*/
