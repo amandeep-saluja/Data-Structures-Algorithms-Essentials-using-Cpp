939ce1bac41430448c46584fbc6d0cce7872cced
amandeep-saluja <amandeepsaluja25@gmail.com>
2022-05-21 23:41:53 +0530

    Day-55

--- Practice/SEction 16 Binary Tree/Level_order.cpp
+++ Practice/SEction 16 Binary Tree/Level_order.cpp
@@ -105,6 +105,19 @@ void levelOrder(Node *root)
     }
 }
 
+int height(Node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+
+    int h1 = height(root->left);
+    int h2 = height(root->right);
+
+    return 1 + max(h1, h2);
+}
+
 int main()
 {
     Node *root = buildLevelOrderTree();
