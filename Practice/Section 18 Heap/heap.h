ed415c28ad6f2b2763ad530cdc23de5bd9bb3107
amandeep-saluja <amandeepsaluja25@gmail.com>
2022-06-05 23:58:00 +0530

    Day-66

--- Practice/Section 18 Heap/heap.h
+++ Practice/Section 18 Heap/heap.h
@@ -4,9 +4,58 @@ using namespace std;
 class Heap
 {
     vector<int> v;
+    bool minHeap;
+
+    bool compare(int a, int b)
+    {
+        if (minHeap)
+        {
+            return a < b;
+        }
+        else
+        {
+            return a > b;
+        }
+    }
+
+    void heapify(int i)
+    {
+        int left = 2 * i;
+        int right = 2 * i + 1;
+
+        int minIdx = i;
+        if (left < v.size() and compare(v[left], v[i]))
+        {
+            minIdx = left;
+        }
+        if (right < v.size() and compare(v[right], v[minIdx]))
+        {
+            minIdx = right;
+        }
+
+        if (minIdx != i)
+        {
+            swap(v[minIdx], v[i]);
+            heapify(minIdx);
+        }
+    }
+
+    /*
+        void heapify(int i)
+        {
+            if (i > v.size())
+                return;
+            int left = 2 * i;
+            int right = 2 * i + 1;
+
+            int minIdx = min(v[left], v[right]);
+            swap(v[minIdx], v[i]);
+            heapify(minIdx);
+        }
+    */
 
 public:
-    Heap(int default_size = 10)
+    Heap(int default_size = 10, bool minHeap = true)
     {
         v.reserve(default_size + 1);
         v.push_back(-1);
@@ -20,11 +69,40 @@ public:
         int idx = v.size() - 1;
         int parent = idx / 2;
 
-        while (idx > 1 and v[idx] < v[parent])
+        while (idx > 1 and compare(v[idx], v[parent]))
         {
             swap(v[idx], v[parent]);
             idx = parent;
             parent = parent / 2;
         }
+    }
+
+    // return min element
+    void top()
+    {
+        return v[1];
+    }
+
+    // Remove the min element
+    void pop()
+    {
+        /*
+            1. swap first and last element  O[1]
+            2. remove last element          O[1]
+            3. Heapify
+        */
+
+        int idx = v.size() - 1;
+
+        swap(v[1], v[idx]);
+
+        v.pop_back();
+
+        heapify(1);
+    }
+
+    bool empty()
+    {
+        return v.size() == 1;
     }
 };
