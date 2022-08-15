b9fca8ed17cd036ea6083e4d6af089b49fa9e3c4
amandeep-saluja <amandeepsaluja25@gmail.com>
2022-08-15 23:41:25 +0530

    Day-72

--- Practice/Section 19 Hashing/HashTable.h
+++ Practice/Section 19 Hashing/HashTable.h
@@ -24,6 +24,21 @@ class HashTable
     int cs; // total entries that have been inserted
     int ts; // size of the table
 
+    int hashFunction(string key)
+    {
+        int idx = 0;
+        int power = 1;
+
+        for (char c : key)
+        {
+            idx = (idx + power * c) % ts;
+            power = (power * 29) % ts;
+        }
+
+        return idx;
+    }
+
+public:
     HashTable(int default_size = 10)
     {
         this->cs = 0;
@@ -34,5 +49,39 @@ class HashTable
         {
             this->table[i] = NULL;
         }
+    }
+
+    void insert(string key, T value)
+    {
+        // Insertion at head of the linked list
+
+        int idx = this->hashFunction(key);
+        Node<T> *n = new Node<T>(key, value);
+        n->next = this->table[idx];
+        this->table[idx] = n;
+        cs++;
+        
+        /*
+        Insertion at tail
+        get the idx through hash function
+        check whether we have a Node created at the idx
+        if so place the value at the end of node
+        else create a new node and pass the address of node to hashtable's idx
+
+        int idx = this->hashFunction(key);
+        if (this->table[idx] != NULL)
+        {
+            Node<T> *temp = this->table[idx];
+            while (temp->next != NULL)
+            {
+                temp = temp->next;
+            }
+            temp->next = new Node<T>(key, value);
+        }
+        else
+        {
+            this->table[idx] = new Node<T>(key, value);
+        }
+        */
     }
 };
