6c7df28ba7e41c8a0ec0c712fc5361da716103e0
amandeep-saluja <amandeepsaluja25@gmail.com>
2022-08-17 19:37:44 +0530

    Day-74

--- Practice/Section 19 Hashing/Ex_65_array_intersection.cpp
+++ Practice/Section 19 Hashing/Ex_65_array_intersection.cpp
@@ -3,25 +3,21 @@ using namespace std;
 
 vector<int> intersection(vector<int> &nums1, vector<int> &nums2)
 {
-
+    // Complexity O[n] + O[n] + O[nlogN] = O[nlogN]
     vector<int> num;
     unordered_set<int> s1;
-    unordered_set<int> s2;
 
     for (int i = 0; i < nums1.size(); i++)
     {
         s1.insert(nums1[i]);
     }
-    for (int i = 0; i < nums2.size(); i++)
-    {
-        s2.insert(nums2[i]);
-    }
 
-    for (int x : s1)
+    for (int x : nums2)
     {
-        if (s2.find(x) != s2.end())
+        if (s1.find(x) != s1.end())
         {
             num.push_back(x);
+            s1.erase(x);
         }
     }
     sort(num.begin(), num.end());
