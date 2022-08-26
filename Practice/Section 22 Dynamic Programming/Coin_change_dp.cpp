0c7ff5129542ad67471e20c4001e2d85aa3111da
amandeep-saluja <amandeepsaluja25@gmail.com>
2022-08-26 13:44:36 +0530

    Day-82

---
+++ Practice/Section 22 Dynamic Programming/Coin_change_dp.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int minCoinChange(vector<int> coins, int amount)
+{
+    vector<int> dp(amount + 1, 0);
+    int t = coins.size();
+
+    for (int i = 1; i <= amount; i++)
+    {
+        dp[i] = INT_MAX;
+        for (int c : coins)
+        {
+            if (i - c >= 0 and dp[i - c] != INT_MAX)
+            {
+                dp[i] = min(dp[i], dp[i - c] + 1);
+            }
+        }
+    }
+
+    return dp[amount] == INT_MAX ? -1 : dp[amount];
+}
+
+int main()
+{
+
+    // vector<int> coins = {1, 3, 7, 10};
+    // int amount = 15;
+    // vector<int> coins = {2};
+    // int amount = 3;
+    // vector<int> coins = {1};
+    // int amount = 0;
+    vector<int> coins = {2};
+    int amount = 4;
+
+    cout << minCoinChange(coins, amount) << endl;
+
+    return 0;
+}
+
+/*
+
+int minCoinChange(vector<int> coins, int amount)
+{
+    vector<int> dp(amount + 1, 0);
+    int t = coins.size();
+
+    for (int i = 1; i <= amount; i++)
+    {
+        int minCoins = amount;
+        for (int j = 0; j < t; j++)
+        {
+            int curr = i - coins[j];
+            if (curr < 0)
+                break;
+
+            curr = dp[curr];
+            minCoins = min(curr, minCoins);
+        }
+        if (minCoins == amount)
+            return -1;
+        dp[i] = minCoins + 1;
+    }
+
+    return dp[amount];
+}
+
+*/
