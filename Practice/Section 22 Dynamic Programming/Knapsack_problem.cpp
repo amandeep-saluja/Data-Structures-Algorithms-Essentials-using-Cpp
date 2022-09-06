6301014ee1819289147faf25b3fc6aba610991c5
amandeep-saluja <amandeepsaluja25@gmail.com>
2022-09-06 23:07:46 +0530

    Day-86

---
+++ Practice/Section 22 Dynamic Programming/Knapsack_problem.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int maxProfit(vector<int> wts, vector<int> prices)
+{
+    return 0;
+}
+
+int main()
+{
+
+    int n = 4, W = 11;
+    vector<int> wts{2, 7, 3, 4};
+
+    vector<int> prices{5, 20, 20, 10};
+
+    cout << maxProfit(wts, prices);
+
+    return 0;
+}
