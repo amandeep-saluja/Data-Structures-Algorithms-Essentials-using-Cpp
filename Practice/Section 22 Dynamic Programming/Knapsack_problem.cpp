155738dbf63b2a2a61994729a74ba57835b5fb3d
Amandeep Singh Saluja <amandeepsaluja25@gmail.com>
2022-09-12 22:43:23 +0530

    Day-87

--- Practice/Section 22 Dynamic Programming/Knapsack_problem.cpp
+++ Practice/Section 22 Dynamic Programming/Knapsack_problem.cpp
@@ -2,7 +2,25 @@
 #include <vector>
 using namespace std;
 
-int maxProfit(vector<int> wts, vector<int> prices)
+// Recursive - Top down approach
+int knapsack(int wts[], int prices[], int N, int W)
+{
+    //base case
+    if(N==0 or W==0) {
+        return 0;
+    }
+
+    //recursive case
+    int inc = 0, exc = 0;
+    if(wts[N-1]<=W) {
+        inc = prices[N-1] + knapsack(wts, prices, N-1, W-wts[N-1]);
+    }
+    exc = knapsack(wts, prices, N-1, W);
+    return max(inc, exc);
+}
+
+//Bottom Up DP
+int knapsackDP(int wts[], int prices[], int N, int W)
 {
     return 0;
 }
@@ -10,12 +28,12 @@ int maxProfit(vector<int> wts, vector<int> prices)
 int main()
 {
 
-    int n = 4, W = 11;
-    vector<int> wts{2, 7, 3, 4};
+    int N = 4, W = 11;
+    int wts[] = {2, 7, 3, 4};
 
-    vector<int> prices{5, 20, 20, 10};
+    int prices[] = {5, 20, 20, 10};
 
-    cout << maxProfit(wts, prices);
+    cout << knapsack(wts, prices, N, W);
 
     return 0;
 }
